52b68521a0bedaecbbdf1316676afbef80389233
Anass Ifrah <ifrah.anass@gmail.com>
2022-06-12 02:30:12 +0200

    initialize smart search project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>&#x2728;Filming smart search&#x2728;</h2>
+      
+    </div>
+  );
+}
+
+export default App;
